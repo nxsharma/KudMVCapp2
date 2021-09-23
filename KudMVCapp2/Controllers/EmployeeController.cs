ed2a2f9bcaebf3fdc15a0153fece0bd68ddd316a
Nikhil Sharma <nikhil.sharma@northcare.com>
2021-09-23 14:51:15 -0500

    first commit

---
+++ KudMVCapp2/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KudMVCapp2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+            return View(employees);
+        }
+
+        [HttpGet]
+        public  ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection formCollection)
+        {
+            foreach(string key in formCollection.AllKeys)
+            {
+                Response.Write("Key = " + key + "     ");
+                Response.Write(formCollection[key]);
+                Response.Write("<br/>");
+            }
+            return View();
+        }
+    }
+    
+}
